e05924698cd95f605258012e7b5814e4c05caa92
eic0 <fecht.daniel@gmail.com>
2024-03-18 13:25:13 +0100

    better logging and consider max message size

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ import os
 from dotenv import load_dotenv
 import time
 import logging
+from logging.handlers import RotatingFileHandler
 
 # configure logging
-logger = logging.getLogger(__name__)
+logFormatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%dT%H:%M:%S")
+logger = logging.getLogger("CVEJoshLogger")
+logger.setLevel(logging.INFO)
 
 load_dotenv()
 
@@ -39,7 +42,6 @@ def get_updated_cves():
     # send get request to get all CVEs
     response = requests.get(base_url, auth=(username, password))
     if response.status_code != 200:
-        print("Error with request:", response.status_code)
         logger.warning("Error with request:", response.status_code)
         return []
 
@@ -58,7 +60,6 @@ def get_detailed_cve_info(cve_id):
     # send get request to get detailed information about a CVE
     detailed_response = requests.get(f"{base_url}/{cve_id}", auth=(username, password))
     if detailed_response.status_code != 200:
-        print(f"Fehler bei der Anfrage für CVE {cve_id}:", detailed_response.status_code)
         logger.warning(f"Fehler bei der Anfrage für CVE {cve_id}:", detailed_response.status_code)
         return None
 
@@ -68,14 +69,11 @@ def search_shodan(search_string):
     try:
         # search for vendor and product
         results = shodan_api.search(f"{search_string}")
-        print(f"Count of found hosts for {search_string}: {results['total']}")
         logger.info(f"Count of found hosts for {search_string}: {results['total']}")
         for result in results['matches']:
-            print(f"{result['ip_str']}:{result['port']} {result['hostnames']} - OS: {result['os']} - {result['timestamp'][:10]} ")
             logger.info(f"{result['ip_str']}:{result['port']} {result['hostnames']} - OS: {result['os']} - {result['timestamp'][:10]} ")
         return results
     except shodan.APIError as e:
-        print(f"Error with Shodan-Search: {e}")
         logger.warning(f"Error with Shodan-Search: {e}")
         return {'total':0}
 
@@ -90,18 +88,47 @@ def send_telegram_message(cve_info, shodan_results):
             message += f"- {result['ip_str']}:{result['port']} {result['hostnames']} - OS: {result['os']} - {result['timestamp'][:10]}\n"
 
     send_text = 'https://api.telegram.org/bot' + telegram_bot_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=HTML&text=' + message
-    requests.get(send_text)
-    logger.info("**Sent message**")
+    response = requests.get(send_text)
+    if response.status_code != 200:
+        logger.error(f"Error while sending Message: {response}")
+    else: 
+        logger.info("**Sent message**")
 
 def send_plain_telegram_message(message):
-    send_text = 'https://api.telegram.org/bot' + telegram_bot_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=HTML&text=' + 'CVE SUMMARY:\n' + message
-    requests.get(send_text)
-    logger.info("**Sent message**")
+    send_text = 'https://api.telegram.org/bot' + telegram_bot_token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=HTML&text=' + message
+    response = requests.get(send_text)
+    if response.status_code != 200:
+        logger.error(f"Error while sending Message: {response}")
+    else: 
+        logger.info("**Sent message**")
+
+def send_cve_summary(message):
+    MAX_LENGTH = 3000
+    message_length = len(message)
+    
+    if message_length <= MAX_LENGTH:
+        # Wenn die Nachricht 3000 Zeichen oder weniger hat, sende sie direkt.
+        send_plain_telegram_message(message)
+    else:
+        # Wenn die Nachricht länger als 3000 Zeichen ist, teile sie in Blöcke.
+        for start in range(0, message_length, MAX_LENGTH):
+            # Erstelle einen Block von maximal 3000 Zeichen.
+            message_block = message[start:start + MAX_LENGTH]
+            # Sende den Block.
+            send_plain_telegram_message(message_block)
 
 def main():
 
-    # define logging file
-    logging.basicConfig(filename='cvejosh.log', level=logging.INFO)
+    # define logging and file rotation
+    fileHandler = RotatingFileHandler("cvejosh.log", maxBytes=5*1024*1024, backupCount=2)
+    fileHandler.setFormatter(logFormatter)
+    logger.addHandler(fileHandler)
+
+    # stream handler to log to console
+    consoleHandler = logging.StreamHandler()
+    consoleHandler.setFormatter(logFormatter)
+    logger.addHandler(consoleHandler)
+
     logger.info("**Started**")
     while True:
         updated_cves = get_updated_cves()
@@ -124,7 +151,6 @@ def main():
                     cve_info["vendor"] = vendor
                     cve_info["product"] = products_str
 
-            print(f"CVE ID: {cve_info['id']}, Vendor: {cve_info['vendor']}, Products: {cve_info['product']}, Summary: {cve_info['summary']}")
             logger.info(f"CVE ID: {cve_info['id']}, Vendor: {cve_info['vendor']}, Products: {cve_info['product']}, Summary: {cve_info['summary']}")
             if upgraded_shodan: # if the usage of vuln is possible
                 shodan_results = search_shodan("vuln:" +  cve_info["id"])
@@ -136,13 +162,12 @@ def main():
                     send_telegram_message(cve_info, shodan_results)
             collected_cves += f"CVE ID: {cve_info['id']},{cve_info['vendor']},{cve_info['product']}\nSummary: {cve_info['summary']}\n-------------------------\n"
         if send_cve_summary:
-            send_plain_telegram_message(collected_cves)
+            send_cve_summary('CVE SUMMARY:\n' + collected_cves)
 
             
         try:
             time.sleep(86400) # wait 24 hours ... not the best solution, but whatever
         except KeyboardInterrupt:
-            print("bye")
             logger.info("Stopped")
             break
 
